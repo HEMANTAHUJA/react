e0e406f13d1feb77d07266c0d1921b6b9349b2f0
HEMANTAHUJA <ahujahemant449@gmaill.com>
2024-09-16 11:02:33 +0530

    react

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,19 @@ import vector from "./assets/download5.jpeg";
 import image1 from "./assets/imag1.jpeg";
 import image2 from "./assets/download7.jpeg";
 import image3 from "./assets/download8.jpeg";
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import Child1 from "./Child1";
+import Login from "./Login"
+import { CountContext } from "./CountProvider"
+import Reducer from "./Reducer"
+import { BrowserRouter, Route, RouterProvider, Routes } from "react-router-dom"
+import Signup from "./Signup"
+import Navbar from "./Navbar"
+// import ContactUs from "./ContactUs";
+import Services from "./Services";
+import AboutUs from "./AboutUs";
+import Router from "./router/Router";
+import Dashboard from "./Dashboard";
 
 
 const data = [
@@ -84,65 +95,83 @@ const data = [
 // },
 
 ]
+function App() {
 
-function App(){
+  // useState
+  const [count , setCount ] = useState(0);
+  const [productId , setProductId] = useState(1);
+  const { setCountValue } = useContext(CountContext);
+  const [apiData , setApiData] = useState([]);
+  const [loading , setLoading] = useState(false);
 
-  // let count = 0;
-  const [count ,setCount ] =useState(0);
-  console.log(count);
-  return (
-    <div className=" text-center">
-      {/* props */}
+  const fetchData = async ()=>{
+    setLoading(true);
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products`);
+      const data = await response.json();
+      console.log(data);
+      setApiData(data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error)
+      setLoading(false);
+    }
+  }
+
+  // useEffect
+
+  useEffect(()=>{
+    // fetchData();
+    // console.log(count)
+    // setCount(prev=>prev+1)
+  },[]);
+
+  // if(loading){
+  //   return (
+  //     <div className="text-3xl text-gray-200 flex justify-center items-center w-full h-screen">Loading...</div>
+  //   )
+  // }
+
+
+return (
+    <div className="w-full">
+      {/* <Navbar /> */}
+
+      <RouterProvider router={Router} />
+      {/* <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes> */}
+      {/* <p className="text-3xl text-white">App</p>
+      <button className="p-2 border-2 text-gray-200 active:bg-gray-200 active:text-gray-700" onClick={()=>{setCountValue((prev)=>prev+1)}}>Update Count Value</button> */}
+      {/* <Child count={count} setCount={setCount} /> */}
+      {/* <Reducer /> */}
+      {/* <button className="border-2 p-2 px-4 border-gray-200 active:bg-gray-700 active:text-gray-200" onClick={()=>{setCount(count+1)}}>+ Count</button>
+      <p className="text-2xl">The Value of Count is : {count}</p>
+      <p className="text-2xl">The Value of product id is : {productId}</p>
+      <button className="border-2 p-2 px-4 border-gray-200 active:bg-gray-700 active:text-gray-200" onClick={()=>{setProductId(productId+1)}}>+ Product Id</button> */}
+     {/* <Login /> */}
+      {/* Props */}
       {/* {
-        data.map((item,i)=>{
+        apiData.map((item ,i)=>{
           return (
-            <Card1 heading={item.heading} classname={item.classname} description={item.description} image={item.image} />
+            <Card1 heading={item.title} description={item.price} image={item.image} />
           )
         })
       } */}
-      <p className="text-4xl text-center font-semibold text-white">The value of count is : {count}</p>
-       <Child1 count={count} setCount={setCount}/>
-      {/* <button className="bg-slate-200 p-2 my-4 active:bg-slate-400 active:shadow-inner" onClick={()=> {
-        setCount(count-1);
-        // count =- 1;p
-        // console.log(count);
-      }}>Dec count</button>
-      <button className="bg-slate-200 p-2 my-4 active:bg-slate-400 active:shadow-inner" onClick={()=>{
-        // count += 1;
-        // console.log(count);
-        setCount(count+1);
-      // }}>Inc count</button> */}
-      
+      {/* <p className="text-4xl font-medium text-white">The Value of count is : {count}</p> */}
+      {/* <Child count={count} setCount={setCount} /> */}
 
-     {/* <div className="flex gap-4">
-     <Card1 heading="Card 1 heading" description="This is card 
-      1 desc" classname = "bg-slate-300"/>
-      <Card1 heading="Card 2 heading" description="This is card 
-      2 desc" classname = "bg-sky-300"/>
-      <Card1 heading="Card 3 heading" description="This is card 
-      3 desc" classname = "bg-green-300"/>
-      <Card1 heading="Card 4 heading" description="This is card 
-      4 desc" classname = "bg-red-300"/>
-     </div> */}
-      {/* <Card1 heading="Card 1 heading" description="This is card 
-      1 desc" classname = "bg-slate-300"/>
-      <Card1 heading="Card 2 heading" description="This is card 
-      2 desc" classname = "bg-sky-300"/>
-      <Card1 heading="Card 3 heading" description="This is card 
-      3 desc" classname = "bg-green-300"/>
-      <Card1 heading="Card 4 heading" description="This is card 
-      4 desc" classname = "bg-red-300"/> */}
-
-<p className="text-4xl text-center font-semibold text-white">The value of count is : {count}</p>
-
+{/* <div className="flex gap-4">
+<Card1 heading="Card 1" description="This is card 1 desc" classname = "bg-slate-300" image={ProductImage1} />
+      <Card1 heading="Card 2" description="This is card 2 desc" classname = "bg-green-300"  image={ProductImage2} />
+      <Card1 heading="Card 3" description="This is card 3 desc" classname = "bg-red-300" image={ProductImage3}  />
+      <Card1 heading="Card 4" description="This is card 4 desc" classname = "bg-blue-300" image={ProductImage4} />
+</div> */}
+{/* <p className="text-4xl font-medium text-white">The Value of count is : {count}</p> */}
       </div>
-
-      
-
-  
-      
   )
 }
-
 
 export default App
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import CountProvider from './CountProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+
+    <CountProvider>
     <App />
+    </CountProvider>
   </StrictMode>,
 )
